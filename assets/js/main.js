0822aba1d7c6d2335679287c2b5be4493f7c9c2e
polinaa06 <bezvinnaya00@bk.ru>
2024-09-03 15:52:24 +0300

    Pr1

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+alert('Я JavaScript');
+
+let surname = prompt('Введите вашу фамилию');
+alert(`Ваша фамилия ${surname}`);
+
+let name = prompt('Введите ваше имя');
+alert(`Ваше имя ${name}`);
+
+let lastname = prompt('Введите ваше отчество');
+alert(`Ваше отчество ${lastname}`);
+
+let isTrue = confirm('Это дествительно ваше ФИО?');
+alert(isTrue);
